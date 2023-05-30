4f4e062a5a589c4dc9f944d98609c99283653a2d
Eloi Jahan <108059693+eloiJhn@users.noreply.github.com>
2023-05-30 14:00:40 +0200

    My Project

---
+++ DeployApp/DeployApp/DeployApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DeployApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DeployApp/DeployApp/DeployApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DeployApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
